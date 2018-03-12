2a5f71207a5b01faa726ae4c716b00c4562a76d0
stloder <scl963@gmail.com>
2018-03-12 10:15:15 -0400

    adding models

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 
+app.use(morgan('dev'));
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+app.render( 'index', {title: 'Hall of Fame', people: people} );
 
 app.use('/', function (req, res, next) {
     morgan('dev');
     //console.log(req.method, '|', res.statusCode);
     next();
 })
+
 
 
 
---
+++ models/index.js
@@ -0,0 +1,2 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
